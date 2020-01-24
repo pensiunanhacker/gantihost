940d2a5f1ea02b39d192bd04aac6b13b55a93e28
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2020-01-24 14:43:47 +0800

    Test

--- gantihost.sh
+++ gantihost.sh
@@ -2,8 +2,6 @@
 #
 #
 mount -o rw,remount /system
-mv /etc/hosts /storage/emulated/0/Download/gantihost/hostori
-mount -o rw,remount /system
-mv /storage/emulated/0/Download/gantihost/hostpubg/hosts /etc
+cp -R /gantihost/hosts /etc
 mount -o ro,remount /system
 #
