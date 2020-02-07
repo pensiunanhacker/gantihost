a02a4122e2d273eb4c6edd05e4c0ae823c7be3c3
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2020-02-07 09:01:12 +0800

    Add files via upload

---
+++ 2.host_mod.v2.sh
@@ -0,0 +1,192 @@
+echo "yt:pensiunanhacker [TeaM]"
+#
+sleep 2
+echo ">>>>>>>>>>>>>>>>>"
+sleep 1
+#
+#
+#ur device must rooted
+#execute when after finished match
+#
+echo "plz wait... processing get in ur system..."
+mount -o rw,remount /system
+rm -rf /system/etc/hosts
+sleep 1
+#
+echo "injecting default host system..."
+echo "... plz wait..."
+#
+echo "
+183.3.255.255 localhost
+183.3.255.254 localhost
+183.3.255.1 localhost
+183.3.255.2 localhost
+183.3.255.3 localhost
+183.3.255.4 localhost
+183.3.255.5 localhost
+183.3.255.6 localhost
+183.3.255.7 localhost
+183.3.255.8 localhost
+183.3.255.9 localhost
+183.3.255.10 localhost
+183.3.255.11 localhost
+183.3.255.12 localhost
+183.3.255.13 localhost
+183.3.255.14 localhost
+183.3.255.15 localhost
+183.3.255.16 localhost
+183.3.255.17 localhost
+183.3.255.18 localhost
+183.3.255.19 localhost
+183.3.255.20 localhost
+183.3.255.21 localhost
+183.3.255.22 localhost
+183.3.255.23 localhost
+183.3.255.24 localhost
+183.3.255.25 localhost
+183.3.255.26 localhost
+183.3.255.27 localhost
+183.3.255.28 localhost
+183.3.255.29 localhost
+183.3.255.30 localhost
+183.3.255.31 localhost
+183.3.255.32 localhost
+183.3.255.33 localhost
+183.3.255.34 localhost
+183.3.255.35 localhost
+183.3.255.36 localhost
+183.3.255.37 localhost
+183.3.255.38 localhost
+183.3.255.39 localhost
+###yt:pensiunanhacker[TeaM]###
+183.3.255.110 localhost
+183.3.255.110 intl.cloud.tencent.com
+183.3.255.110 intl.mtp.tencent.com
+183.3.255.110 mtp@tencent.com
+
+###yt:pensiunanhacker[TeaM]###
+127.1.40.2 localhost
+127.0.0.1 localhost.localdomain
+0.0.0.0 local
+255.255.255.255 broadcasthost
+::1 localhost
+::1 ip6-localhost
+::1 ip6-loopback
+fe80::1%lo0 localhost
+ff00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+###yt:pensiunanhacker[TeaM]###
+183.3.255.110 .net
+183.3.255.110 .com
+183.3.255.110 .org
+183.3.255.110 .cn
+183.3.255.110 .cn.net
+###yt:pensiunanhacker[TeaM]###
+0.0.0.0 .qq.com
+0.0.0.0 .helpshift.com
+0.0.0.0 .helpshift.comsubdomain?
+0.0.0.0 .database.helpshift.com
+0.0.0.0 .db.helpshift.com
+0.0.0.0 .demo.helpshift.com
+0.0.0.0 .dev.helpshift.com
+0.0.0.0 .devel.helpshift.com
+0.0.0.0 .developers.helpshift.com
+0.0.0.0 .dmarc.helpshift.com
+0.0.0.0 .dns.helpshift.com
+0.0.0.0 .dns1.helpshift.com
+0.0.0.0 .dns10.helpshift.com
+0.0.0.0 .dns2.helpshift.com
+0.0.0.0 .dns20.helpshift.com
+0.0.0.0 .dns3.helpshift.com
+0.0.0.0 .dns4.helpshift.com
+0.0.0.0 .dns5.helpshift.com
+0.0.0.0 .dns6.helpshift.com
+0.0.0.0 .dns7.helpshift.com
+0.0.0.0 .dns8.helpshift.com
+0.0.0.0 .dns9.helpshift.com
+0.0.0.0 .dots.helpshift.com
+0.0.0.0 .download.helpshift.com
+###yt:pensiunanhacker[TeaM]###
+
+127.0.0.0 ns.chinanet.cn
+::1 localhost
+::1 ip6-localhost
+::1 ip6-loopback
+fe80::1%lo0 localhost
+ff00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+2001:da8:201:1386:f49d:cbb6:da93:e81f			  2014 2001:da8:201:1386:f49d:cbb6:da93:e81f, CHN, 1
+2001:da8:7000:8:bc82:c821:de2c:70c1			  2014 2001:da8:7000:8:bc82:c821:de2c:70c1, CHN, 1
+2001:da8:201:1386:914:f1d:a69f:ed2e			  2014 2001:da8:201:1386:914:f1d:a69f:ed2e, CHN, 1
+2001:da8:201:1386:1ff:2cc3:f2ec:18a7			  2014 2001:da8:201:1386:1ff:2cc3:f2ec:18a7, CHN, 1
+2001:da8:201:1386:84a3:8783:578a:5f03			  2014 2001:da8:201:1386:84a3:8783:578a:5f CHN, 1
+2001:da8:201:1386:f49d:cbb6:da93:e81f			  2014 2001:da8:201:1386:f49d:cbb6:da93:e81f, CHN, 1
+2001:da8:7000:8:bc82:c821:de2c:70c1			  2014 2001:da8:7000:8:bc82:c821:de2c:70c1, CHN, 1
+2001:2e8:602::2:1:0:30			  2014 v6-2001-2e8-602-0-2-1-0-30.ub-freebit.net, JPN, 1
+2001:da8:201:1386:914:f1d:a69f:ed2e			  2014 2001:da8:201:1386:914:f1d:a69f:ed2e, CHN, 1
+2001:da8:201:1386:1ff:2cc3:f2ec:18a7			  2014 2001:da8:201:1386:1ff:2cc3:f2ec:18a7, CHN, 1
+2001:da8:201:1386:84a3:8783:578a:5f03			  2014 2001:da8:201:1386:84a3:8783:578a:5f CHN, 1
+2001:da8:201:1386:bc29:19cc:cad6:33a7			  2014 2001:da8:201:1386:bc29:19cc:cad6:33a7, CHN, 1
+2001:da8:201:1386:6df8:4a02:5dd0:239e			  2014 2001:da8:201:1386:6df8:4a02:5dd0:239e, CHN, 1
+2001:da8:201:1147:1c7e:5392:b832:d34f			  2014 2001:da8:201:1147:1c7e:5392:b832:d34f, CHN, 1
+2001:da8:200:9002:7c7f:69da:4ad7:ff80			  2014 2001:da8:200:9002:7c7f:69da:4ad7:ff80, CHN, 1
+2001:da8:201:1147:988:c6f8:d279:5d93			  2014 2001:da8:201:1147:988:c6f8:d279:5d93, CHN, 1
+2001:da8:200:9002:30e5:fff8:19:f620			  2014 2001:da8:200:9002:30e5:fff8:19:f6 CHN, 1
+###yt:pensiunanhacker[TeaM]###
+127.0.0.0 ns.chinanet.cn
+127.0.0.0 189.cn
+###yt:pensiunanhacker[TeaM]###
+" > /system/etc/hosts
+chmod  -R 777 /system/etc/hosts
+mount -o ro,remount /system
+echo "ok done injected ur host system"
+##
+##
+echo "plz wait... processing get in ur system..."
+mount -o rw,remount /system
+rm -rf /system/etc/resolv.conf
+sleep 1
+#
+echo "injecting default DNS system..."
+echo "... plz wait..."
+#
+echo "
+nameserver 8.8.4.4
+nameserver 8.8.8.8
+" > /system/etc/resolv.conf
+chmod  -R 777 /system/etc/resolv.conf
+mount -o ro,remount /system
+echo "done DNS system"
+#
+#
+echo ""
+echo "iptables inject domain..."
+sleep 2
+iptables -I INPUT -s voice.gcloudcs.com -j DROP &>/dev/null
+iptables -I INPUT -s vmp.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s sg.tdm.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s intldlgs.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s cdn.wetest.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s android.bugly.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s receiver.sg.tdm.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s hkconfig.gcloud.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s idcconfig.gcloud.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s napubgm.broker.tplay.qq.com -j DROP &>/dev/null
+iptables -I INPUT -s csoversea.mbgame.gamesafe.qq.com -j DROP &>/dev/null
+echo "done inject IP domain system"
+sleep 2
+#
+echo "done complete"
+echo ""
+echo "KEEP IT COMMAND STILL ON"
+#
+#
+#
+
+
